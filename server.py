28fe375da5f3939e9a72040dd46668400748de5a
KielRodriguez <kr.rdz.20@gmail.com>
2019-04-18 22:39:10 -0500

    config swagger with people endpoint

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
-from flask import (
-  Flask,
-  render_template
-)
+from flask import render_template
+import connexion
 
 # Create the application instance
-app = Flask(__name__, template_folder="templates")
+app = connexion.App(__name__, specification_dir='./')
+
+# Read the swanger.yml file to configure endpoints
+app.add_api('swagger.yml')
 
 # Create URL route in our application for "/"
 @app.route('/')
